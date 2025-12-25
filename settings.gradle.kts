855d7aad290eaad380ba145f12de0dc5cffca938
thumb2086 <thumb2087@gmail.com>
2025-12-25 22:10:33 +0800

    chore: Upgrade foojay-resolver-convention plugin to v1.0.0
    This commit updates the `foojay-resolver-convention` plugin from version `0.7.0` to `1.0.0` in `settings.gradle.kts`.
    
    Additionally, the `@Suppress("UnstableApiUsage")` annotation has been added to handle new API usage requirements.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,10 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
